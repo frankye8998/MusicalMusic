a6366a1e9b4dbc98f02fbeeb1b6b5929fd6b251a
Frank Ye <frankye8998@gmail.com>
2019-01-13 19:29:03 -0500

    Added descriptions to search

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="MusicalMusic",
-  version="0.1.0",
+  version="0.1.1",
   description="Retrieve Sheet music from Musescore!",
   long_description=open("README.md").read(),
   long_description_content_type='text/markdown',
