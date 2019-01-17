a5c4e56d9f3807bc837da6ee7b76051f5d80617f
Frank Ye <frankye8998@gmail.com>
2019-01-16 19:44:10 -0500

    PEP 8

---
+++ MusicalMusic/exceptions.py
@@ -0,0 +1,15 @@
+class MuseScoreException(Exception):
+    pass
+
+
+class InvalidFileExtension(MuseScoreException):
+    pass
+
+
+class InvalidScoreID(MuseScoreException):
+    pass
+
+
+class InvalidCredentials(MuseScoreException):
+    pass
+
