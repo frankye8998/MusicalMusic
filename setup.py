c4aa1aa4b2d8d19c54ea970daf4a8a095a00a4c0
Frank Ye <frankye8998@gmail.com>
2019-04-02 19:42:57 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="MusicalMusic",
-  version="0.2.1",
+  version="0.2.2",
   description="Retrieve Sheet music from Musescore!",
   long_description=open("README.md").read(),
   long_description_content_type='text/markdown',
