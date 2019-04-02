a6ba2ce13e9ca3c38b22de74ee251cd9d0fd081b
Frank Ye <frankye8998@gmail.com>
2019-04-01 23:43:28 -0400

    Added download function

--- MusicalMusic/main.py
+++ MusicalMusic/main.py
@@ -12,7 +12,7 @@ class MusicalMusic:
     def __init__(self, username, password):
 
         url = "https://musescore.com/user/login"
-        r = requests.get(url)
+        r = requests.get(url, verify=False)
         soup = bs4.BeautifulSoup(r.text, "html.parser")
         csrf = soup.find("meta", {"name": "csrf-token"})["content"]
         url = "https://musescore.com/user/auth/login/process"
@@ -30,7 +30,8 @@ class MusicalMusic:
             mu_user = requests.post(url,
                                     data=data,
                                     cookies=cookies,
-                                    allow_redirects=False
+                                    allow_redirects=False,
+                                    verify=False,
                                     ).cookies["mu_user_new"]
         except KeyError as e:
             raise InvalidCredentials(
@@ -49,7 +50,21 @@ class MusicalMusic:
         newlink = f"https://musescore.com/score/{id}/download/{format}"
         cookies = {"mu_browser_uni": self.mu_browser_uni,
                    "mu_user_new": self.mu_user}
-        bytes = requests.get(newlink, cookies=cookies)
+        bytes = requests.get(newlink, cookies=cookies, verify=False)
         if bytes.status_code != 200:
             raise InvalidScoreID(str(bytes.status_code))
         return bytes.content
+
+    def download(self, id, filename, format="mp3"):
+        if format not in ["mp3", "pdf", "mid", "mxl", "mscz"]:
+            raise InvalidFileExtension("Must be mp3, pdf, mid, mxl, or mscz.")
+        newlink = f"https://musescore.com/score/{id}/download/{format}"
+        opener = urllib.request.build_opener()
+        opener.addheaders = [("cookie", f"mu_browser_uni={self.mu_browser_uni};
+                             mu_user_new={self.mu_user}")]
+        urllib.request.install_opener(opener)
+        try:
+            urllib.request.urlretrieve(newlink, filename)
+        except urllib.error.HTTPError:
+            raise InvalidScoreID("The ID of the score is invalid!")
+
